4c019a07b86525d1a4c7fb17255e35415726b754
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-01-16 17:30:39 +0000

    updated

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,38 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT, 
+email TEXT,
+address TEXT,
+phone_number TEXT,
+Company_ID INTEGER
+); 
+
+
+CREATE TABLE salespersons (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT, 
+email address TEXT,
+); 
+
+
+CREATE TABLE companies (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+Name TEXT,
+); 
+
+
+CREATE TABLE activities (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+contact_id INTEGER,
+Calls TEXT,
+Email TEXT, 
+date address TEXT,
+time TEXT,
+Notes INTEGER
+); 
